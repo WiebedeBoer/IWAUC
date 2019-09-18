e8793a58fcb1467ad12b5c5f5f35dee24179f296
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2019-09-18 11:12:52 +0200

    versions print
    versions print

---
+++ importTensor.py
@@ -0,0 +1,16 @@
+import numpy
+print('numpy ' + numpy.__version__)
+import cv2
+print ('opencv ' + cv2.__version__)
+import pandas as pd
+print('pandas ' + pd.__version__)
+import scipy
+print('scipy ' + scipy.__version__)
+import keras
+print('keras ' +keras.__version__)
+import matplotlib
+print ('matplotlib ' + matplotlib.__version__)
+import tensorflow as tf
+print('tensorflow ' + tf.__version__)
+
+
